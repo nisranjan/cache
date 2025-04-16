25f26a9540f4de8cfafe9d7cb69f21bd4c040fa1
Nishant Ranjan <nishant.ranjan@gmail.com>
2025-04-16 20:02:53 +0530

    Completed the BasicClient which runs from a main() method

--- src/main/java/nisran/BasicClient.java
+++ src/main/java/nisran/BasicClient.java
@@ -1,25 +1,30 @@
 /**
- * 
+ * A basic client implementation to test the LRU Cache
  */
-package nisran.cache;
+package nisran;
 
+import nisran.cache.LRUCache;
 
 /**
  * A basic client implementation to test the LRU Cache
  */
-public class BasicCacheClient {
+public class BasicClient {
 	
 	static LRUCache<Integer,String> cache;
+	static Integer key;
+	static String value;
 
 	static void init(Integer capacity) {
-		BasicCacheClient.cache = new LRUCache<Integer,String>(capacity);
+		BasicClient.cache = new LRUCache<Integer,String>(capacity);
 	}
 	
 	static void testSet(Integer key, String value) {
 		
 		cache.set(key, value);
+		BasicClient.key = key;
+		BasicClient.value = value;
 		//Cache should contain the key-value
-		assert (value == cache.get(key));
+		assert (BasicClient.value == cache.get(key));
 		System.out.println("\n ******* Inside testSet(K,V) *******");
 		System.out.println("Cache contains key " + key );
 		//Cache head should be the current key
@@ -30,7 +35,7 @@ public class BasicCacheClient {
 	
 	static void testGet(Integer key) {
 		
-		assert(cache.list.size() != 0);
+		//assert(cache.list.size() != 0);
 		
 		String value = cache.get(key);
 		
@@ -40,7 +45,7 @@ public class BasicCacheClient {
 		}else {
 			//Node should be head after SEARCH
 			//Testing is not correct, Assertions are not working
-			assert(value == cache.dict.get(cache.list.getFirst()));
+			assert(value == BasicClient.value);
 			System.out.println("\n ******* Inside testGet(K) *******");
 			System.out.println("Value at head is  " + value );
 		}
@@ -59,13 +64,13 @@ public class BasicCacheClient {
 	 */
 	public static void main(String[] args) {
 		
-		BasicCacheClient.init(1);
+		BasicClient.init(1);
 		//BasicCacheClient.testGet("Nishant".hashCode());
-		BasicCacheClient.testSet("First String".hashCode(), "First String");
-		BasicCacheClient.testGet("First String".hashCode());
-		BasicCacheClient.testSet("Second String".hashCode(), "Second String");
-		BasicCacheClient.testGet("First String".hashCode());
-		BasicCacheClient.testGet("Second String".hashCode());
+		BasicClient.testSet("First String".hashCode(), "First String");
+		BasicClient.testGet("First String".hashCode());
+		BasicClient.testSet("Second String".hashCode(), "Second String");
+		BasicClient.testGet("First String".hashCode());
+		BasicClient.testGet("Second String".hashCode());
 	}
 
 }
