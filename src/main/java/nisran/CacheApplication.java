e9ce7edb3032d016a455aefb032533359080ef1d
Nishant Ranjan <nishant.ranjan@gmail.com>
2025-04-29 16:00:20 +0530

    modifications which allowed a single server to run correctly in the cloud environment

--- src/main/java/nisran/CacheApplication.java
+++ src/main/java/nisran/CacheApplication.java
@@ -3,7 +3,9 @@ package nisran;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import io.awspring.cloud.autoconfigure.sqs.SqsAutoConfiguration;
+
+@SpringBootApplication(exclude = {SqsAutoConfiguration.class})
 public class CacheApplication {
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
--- src/main/java/nisran/discovery/ServiceDiscoveryConfig.java
+++ src/main/java/nisran/discovery/ServiceDiscoveryConfig.java
@@ -3,8 +3,12 @@ package nisran.discovery;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.servicediscovery.ServiceDiscoveryClient;
 import software.amazon.awssdk.services.ecs.EcsClient;
+import software.amazon.awssdk.services.servicediscovery.ServiceDiscoveryClientBuilder;
+import software.amazon.awssdk.services.ecs.EcsClientBuilder;
 
 @Configuration
 @Profile("cluster")
@@ -12,11 +16,19 @@ public class ServiceDiscoveryConfig {
 
     @Bean
     public ServiceDiscoveryClient serviceDiscoveryClient() {
-        return ServiceDiscoveryClient.builder().build();
+        ServiceDiscoveryClientBuilder builder = ServiceDiscoveryClient.builder();
+        return builder
+            .region(Region.AP_SOUTH_1)
+            .credentialsProvider(DefaultCredentialsProvider.create())
+            .build();
     }
 
     @Bean
     public EcsClient ecsClient() {
-        return EcsClient.builder().build();
+        EcsClientBuilder builder = EcsClient.builder();
+        return builder
+            .region(Region.AP_SOUTH_1)
+            .credentialsProvider(DefaultCredentialsProvider.create())
+            .build();
     }
 } 
