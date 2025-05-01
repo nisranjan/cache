7949264460d1cb912cb7249e9ee081394fc1b938
Nishant Ranjan <nishant.ranjan@gmail.com>
2025-05-01 19:55:17 +0530

    resolved error java.lang.NoSuchFieldError: endpointProvider in cluster profile

--- src/main/java/nisran/CacheApplication.java
+++ src/main/java/nisran/CacheApplication.java
@@ -3,9 +3,8 @@ package nisran;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.awspring.cloud.autoconfigure.sqs.SqsAutoConfiguration;
 
-@SpringBootApplication(exclude = {SqsAutoConfiguration.class})
+@SpringBootApplication
 public class CacheApplication {
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
